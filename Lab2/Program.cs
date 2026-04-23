3bd031e4fcd8c3ffa0952adffa61f99055a61d64
Rich182182 <zhenu1276@gmail.com>
2026-04-23 18:42:22 +0300

    done 1,2

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Вербицький Є. В.");
+
+        Random rand = new Random();
+        string[] nums = new string[3];
+
+        Console.WriteLine("Згенеровані числа для першого завдання:");
+        for (int i = 0; i < 3; i++)
+        {
+            nums[i] = rand.Next(10, 10000).ToString();
+            Console.Write(nums[i] + " ");
+        }
+        Console.WriteLine();
+
+        char maxDigit = '0';
+        for (int i = 0; i < 3; i++)
+        {
+            if (nums[i][0] > maxDigit)
+            {
+                maxDigit = nums[i][0];
+            }
+        }
+        Console.WriteLine("Найбільша перша цифра: " + maxDigit);
+
+        string text = "Текст для перевірки коду. Тут є великі слова і малі.";
+        Console.WriteLine("\nПочатковий текст для другого завдання:");
+        Console.WriteLine(text);
+
+        string[] words = text.Split(new[] { ' ', '.', ',', '!', '?', ':', ';' }, StringSplitOptions.RemoveEmptyEntries);
+
+        int minLen = int.MaxValue;
+        foreach (string w in words)
+        {
+            if (w.Length < minLen)
+            {
+                minLen = w.Length;
+            }
+        }
+
+        Console.WriteLine("\nСлова мінімальної довжини:");
+        foreach (string w in words)
+        {
+            if (w.Length == minLen)
+            {
+                Console.WriteLine(w);
+            }
+        }
+    }
+}
--- MRPZ/Program.cs
+++ MRPZ/Program.cs
@@ -1,2 +1,79 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Вербицький Є. В.");
+
+        Console.Write("Введіть розмір матриці n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int[,] a = new int[n, n];
+
+        Console.WriteLine("Введіть елементи матриці по рядках:");
+        for (int i = 0; i < n; i++)
+        {
+            string[] input = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            for (int j = 0; j < n; j++)
+            {
+                a[i, j] = int.Parse(input[j]);
+            }
+        }
+
+        Console.WriteLine("\nМатриця А:");
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(a[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+
+        int sum = 0;
+        for (int j = 0; j < n; j++)
+        {
+            sum += a[0, j];
+        }
+
+        bool isMagic = true;
+
+        for (int i = 0; i < n; i++)
+        {
+            int rowSum = 0;
+            int colSum = 0;
+            for (int j = 0; j < n; j++)
+            {
+                rowSum += a[i, j];
+                colSum += a[j, i];
+            }
+            if (rowSum != sum || colSum != sum)
+            {
+                isMagic = false;
+            }
+        }
+
+        int diag1 = 0;
+        int diag2 = 0;
+        for (int i = 0; i < n; i++)
+        {
+            diag1 += a[i, i];
+            diag2 += a[i, n - 1 - i];
+        }
+
+        if (diag1 != sum || diag2 != sum)
+        {
+            isMagic = false;
+        }
+
+        if (isMagic)
+        {
+            Console.WriteLine("Магічне число: " + sum);
+        }
+        else
+        {
+            Console.WriteLine("Матриця не є магічним квадратом.");
+        }
+    }
+}
